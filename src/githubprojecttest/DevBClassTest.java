d391f6bfc50b7053f96759506bdd8839d7879277
Zaim hasan <zaimhasanchy@gmail.com>
2023-11-06 12:55:33 +0600

    commit 2 from dev B

--- src/githubprojecttest/DevBClassTest.java
+++ src/githubprojecttest/DevBClassTest.java
@@ -3,5 +3,6 @@ package githubprojecttest;
 public class DevBClassTest {
     public static void main(String[] args) {
         System.out.println("Commit 1 from dev B");
+        System.out.println("Commit 2 from dev B");
     }
 }
